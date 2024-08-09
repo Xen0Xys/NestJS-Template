6e2f1d7379a6eab2e864e7ec2f8fb1907504d25d
Xen0Xys <red57101@gmail.com>
2024-08-09 13:56:35 +0200

    :art: Remove tests and improve global structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,32 @@
-import {Module} from "@nestjs/common";
-import {AuthModule} from "./auth/auth.module";
+import {MailerModule} from "@nestjs-modules/mailer";
+import {ThrottlerModule} from "@nestjs/throttler";
 import {UsersModule} from "./users/users.module";
+import {AuthModule} from "./auth/auth.module";
+import {ConfigModule} from "@nestjs/config";
+import {Module} from "@nestjs/common";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 @Module({
-    imports: [UsersModule, AuthModule]
+    imports: [
+        ConfigModule.forRoot({isGlobal: true}),
+        ThrottlerModule.forRoot([{
+            ttl: 60000,
+            limit: 50,
+        }]),
+        MailerModule.forRoot({
+            transport: {
+                host: process.env.EMAIL_HOST,
+                port: parseInt(process.env.EMAIL_PORT),
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.EMAIL_PASSWORD,
+                }
+            },
+        }),
+        UsersModule,
+        AuthModule
+    ]
 })
 export class AppModule{}

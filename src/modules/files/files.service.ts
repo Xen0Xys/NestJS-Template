997ada68cf548c8e7cb307446704e0e81a2a2e80
Xen0Xys <red57101@gmail.com>
2024-08-10 14:58:31 +0200

    :sparkles: Add S3 file storage support

--- src/modules/files/files.service.ts
+++ src/modules/files/files.service.ts
@@ -4,6 +4,7 @@ import {CipherService} from "../../common/services/cipher.service";
 import {ConfigService} from "@nestjs/config";
 import {Saver} from "./savers/saver";
 import {FileSaver} from "./savers/file.saver";
+import {S3Saver} from "./savers/s3.saver";
 
 @Injectable()
 export class FilesService{
@@ -15,7 +16,18 @@ export class FilesService{
         private readonly prismaService: PrismaService,
         private readonly cipherService: CipherService,
     ){
-        this.saver = new FileSaver(this.configService.get("UPLOADS_PATH"));
+        if(this.configService.get("FILESYSTEM") === "s3")
+            this.saver = new S3Saver(
+                this.configService.get("S3_ENDPOINT"),
+                this.configService.get("S3_PORT"),
+                this.configService.get("S3_USE_SSL") === "true",
+                this.configService.get("S3_REGION"),
+                this.configService.get("S3_ACCESS_KEY"),
+                this.configService.get("S3_SECRET_KEY"),
+                this.configService.get("S3_BUCKET_NAME")
+            );
+        else
+            this.saver = new FileSaver(this.configService.get("UPLOADS_PATH"));
     }
 
     async uploadFile(userId: number, data: Buffer, fileName: string, size: number){
